1469ae891bf8b97e07784abb55b16cbf14135151
EDaraya <65674097+EDaraya@users.noreply.github.com>
2020-09-18 12:55:23 -0600

    Código Flake8 corregido

---
+++ Prueba_Flake8_corregido.py
@@ -0,0 +1,11 @@
+def func(a, b, c):
+    if c == 0:
+        d = a+b
+        return d
+    if c == 1:
+        d = a-b
+        return d
+    if c == 2:
+        d = a*b
+        return d
+    return "eror"

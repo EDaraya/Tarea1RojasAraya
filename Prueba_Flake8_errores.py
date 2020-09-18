43ccddb83322c85f3ac187c6b3309aa51ccd0ba8
EDaraya <65674097+EDaraya@users.noreply.github.com>
2020-09-18 12:53:23 -0600

    Código Flake8 errores

---
+++ Prueba_Flake8_errores.py
@@ -0,0 +1,15 @@
+def func(a,b,c):
+    
+    #uno
+    
+    if c == 0:
+        #uno
+        d = a+b
+        return d
+    if c == 1:
+        d = a-b
+        return d
+    if c == 2:
+        d = a*b
+        return d
+    return "eror"
